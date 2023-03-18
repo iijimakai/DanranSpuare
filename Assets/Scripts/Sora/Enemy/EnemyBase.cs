fb69d659cde3e3d1e2419afc4bcbfcec29fe5648
hinata0224 <hinata.act.2@gmail.com>
2023-03-18 11:57:22 +0900

    :heavy_plus_sign: 2Dの追加

---
+++ Assets/Scripts/Sora/Enemy/EnemyBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class EnemyBase : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
