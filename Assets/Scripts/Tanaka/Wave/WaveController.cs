30da0e2a2cd242ce4ef48177433ba972f485b52e
LAPTOP-R9GPHUCK\keisu <keisuke20151114@gmail.com>
2023-05-27 14:24:45 +0900

    WaveController仮実装 WaveControllerのリファクタリングや関数の説明の追加など Enemy4の追尾システムのリファクタリング

--- Assets/Scripts/Tanaka/Wave/WaveController.cs
+++ Assets/Scripts/Tanaka/Wave/WaveController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,14 +44,13 @@ namespace wave
         private CompositeDisposable enemySubscriptions = new CompositeDisposable();
         [SerializeField] private PlayerInputTest playerObject;
         private ReactiveProperty<int> totalActiveEnemies = new ReactiveProperty<int>(0);
-        // 現在アクティブな敵の数を記録するカウンター
-        //private int totalActiveEnemies = 0;
+        private bool allEnemiesSpawned = false; // 追加: ウェーブ内の全ての敵がスポーンしたかどうかを示すフラグ
         int tempCount = 0;
 
         [SerializeField]
         private int maxActiveEnemies;
         /// <summary>
-        /// 最初のウェーブを生成。
+        /// ゲーム開始時に呼ばれ、ウェーブと敵の初期化を行います。
         /// </summary>
         void Start()
         {
@@ -75,10 +75,17 @@ namespace wave
                 .Subscribe(async _ => await SpawnWave(cancelToken.Token));
                 //Observable.NextFrame().Subscribe(async _ => await SpawnWave(cancelToken.Token));
         }
+        /// <summary>
+        /// 指定されたウェーブの敵をスポーン。
+        /// </summary>
+        /// <param name="token">非同期タスクのキャンセルトークン</param>
+        /// <returns>ウェーブのスポーンが完了したら完了するタスク</returns>
         private async Task SpawnWave(CancellationToken token)
         {
             if(currentWaveIndex < waves.Length)
             {
+                // 新しいWaveの敵が生成される前に一定時間の遅延を導入する
+                await Task.Delay(TimeSpan.FromSeconds(3), token);
                 Wave currentWave = waves[currentWaveIndex];
                 int i = 0;
                 while(i < currentWave.totalEnemies)
@@ -103,40 +110,48 @@ namespace wave
                         SpawnEnemy(chosenEnemy);
                         chosenEnemy.currentSpawnCount++;
                         await Task.Delay(2000, token);//.ConfigureAwait(false); // エラー通知を無効にする
-                        i++;
                     }
+                    i++;
                 }
+                await totalActiveEnemies
+                    .Where(activeEnemies => activeEnemies == 0)
+                    .First()
+                    .ToTask(token);
+                //allEnemiesSpawned = false; // フラグをリセット
                 currentWaveIndex++;
-                await Task.Delay(2000, token);//.ConfigureAwait(false); // エラー通知を無効にする
+                Debug.Log(currentWaveIndex);
+                // 全ての敵がスポーンした後に遅延を入れてからフラグを更新
+                allEnemiesSpawned = true; // 全ての敵がスポーンしたのでフラグを更新
+                Debug.Log("allEnemiesSpawned;"+allEnemiesSpawned);
             }
         }
 
         /// <summary>
-        /// 生成する敵のたいぷのインデックスを、その生成確率に基づいてかえす。
+        /// 敵のタイプのインデックスをランダムに選択。
+        /// 選択は敵のスポーン確率に基づく。
         /// </summary>
-        /// <param name="enemies">生成可能な敵のタイプの配列。</param>
-        /// <returns>生成する敵のタイプのインデックス。</returns>
+        /// <param name="enemies">敵のタイプの配列</param>
+        /// <returns>選択された敵のタイプのインデックス</returns>
         private int GetRandomEnemyIndex(EnemyType[] enemies)
         {
             float total = 0;
-            float randomPoint = Random.value;
+            float randomPoint = UnityEngine.Random.value;
 
             for (int i = 0; i < enemies.Length; i++)
             {
                 total += enemies[i].spawnChance;
                 if (randomPoint <= total)
                 {
-                    Debug.Log(i);
+                    //Debug.Log(i);
                     return i;
                 }
             }
             throw new System.Exception("a");
-            //return enemies.Length - 1;
         }
         /// <summary>
-        /// 指定したタイプの敵を1体生成する。
+        /// 指定されたタイプの敵をスポーンします。
         /// </summary>
-        /// <param name="enemyType">生成する敵のタイプ。</param>
+        /// <param name="enemyType">スポーンさせる敵のタイプ</param>
         private void SpawnEnemy(EnemyType enemyType)
         {
             GameObject spawnedEnemyObject = enemyPools[enemyType.enemyPrefab].GetObject(); // プールから敵を取得
@@ -146,35 +161,51 @@ namespace wave
                 Debug.LogError("Enemy object is null.");
                 return;
             }
-            IEnemy spawnedEnemy = spawnedEnemyObject.GetComponent<IEnemy>();
             totalActiveEnemies.Value++;
+            Debug.Log("Total Active Enemies after spawn: " + totalActiveEnemies.Value);
+            IEnemy spawnedEnemy = spawnedEnemyObject.GetComponent<IEnemy>();
             //敵の出現位置をランダムに設定
             //ここでスポーン位置を設定
             float spawnRadius = 15.0f;
             Vector3 playerPosition = playerObject.transform.position;
             Vector3 spawnPosition = playerPosition + new Vector3(
-                Random.Range(-spawnRadius,spawnRadius),
-                Random.Range(-spawnRadius,spawnRadius),
-                Random.Range(-spawnRadius,spawnRadius)
+                UnityEngine.Random.Range(-spawnRadius,spawnRadius),
+                UnityEngine.Random.Range(-spawnRadius,spawnRadius),
+                UnityEngine.Random.Range(-spawnRadius,spawnRadius)
             );
             spawnedEnemyObject.transform.position = spawnPosition;
             //敵が破壊されたときにプールに戻るように設定
             spawnedEnemy.OnDestroyed.Subscribe(async _ =>
             {
-                // 敵が破壊されたので、カウンターを減らす
-                totalActiveEnemies.Value--;
-                spawnedEnemyObject.SetActive(false);
-                tempCount++;
-                Debug.Log("DeathCount" + tempCount);
-                if (totalActiveEnemies.Value == 0)
-                {
-                    Debug.Log("All Enemies Destroy");
-                    Debug.Log("NextWave");
-                    await SpawnWave(cancelToken.Token);
-                }
-                spawnedEnemy.ResetSubscription();
+                DestroyEnemy(spawnedEnemyObject, spawnedEnemy);
             }).AddTo(this);
         }
+            /// <summary>
+        /// 指定された敵オブジェクトを破壊します。
+        /// </summary>
+        /// <param name="enemyObject">破壊する敵オブジェクト</param>
+        /// <param name="enemy">破壊する敵</param>
+        private async void DestroyEnemy(GameObject enemyObject, IEnemy enemy)
+        {
+            totalActiveEnemies.Value--;
+            Debug.Log("Total Active Enemies after destroy: " + totalActiveEnemies.Value);
+            //
+            if (allEnemiesSpawned && totalActiveEnemies.Value == 0)
+            {
+                Debug.Log("All Enemies Destroy");
+                Debug.Log("NextWave");
+                allEnemiesSpawned = false; // フラグをリセット
+                Debug.Log("allEnemiesSpawned" + allEnemiesSpawned);
+                await SpawnWave(cancelToken.Token);
+            }
+            enemyObject.SetActive(false);
+            tempCount++;
+            Debug.Log("DeathCount" + tempCount);
+            enemy.ResetSubscription();
+        }
+        /// <summary>
+        /// ゲームオブジェクトが無効になったときに呼び出され、進行中のウェーブのスポーンをキャンセルする処理。
+        /// </summary>
         void OnDisable()  // ゲーム停止時に呼び出されるメソッド
         {
             cancelToken.Cancel();  // 非同期タスクを停止する
