6bf58e7c37c42bf99a0c64996159de13c8d2438f
chocomint <keisuke20151114@gmail.com>
2023-07-22 11:04:43 +0900

    Enemy1のエラー修正完了

--- Assets/Scripts/Sora/Enemy/EnemyBase.cs
+++ Assets/Scripts/Sora/Enemy/EnemyBase.cs
@@ -78,8 +78,11 @@ namespace Enemy
             this.UpdateAsObservable()
                 .Subscribe(_ => LockPlayer(player))
                 .AddTo(moveDispose);
+                                Debug.Log("DeadSubscription");
+
             deadFlag.Subscribe(_ => Dead())
                 .AddTo(disposables);
+                Debug.Log("DeadSubscription");
         }
 
         /// <summary>
